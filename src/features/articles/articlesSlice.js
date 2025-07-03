740fa021f54f4743e16dfb9c7c648984712cdc25
Ashok naik <ashok@yourstory.com>
2025-07-03 15:50:13 +0530

    Update API_HOST to latest Ngrok URL

--- src/features/articles/articlesSlice.js
+++ src/features/articles/articlesSlice.js
@@ -275,7 +275,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 // TODO: Move to Env file
-const API_HOST = 'https://1ffa-106-51-159-190.ngrok-free.app';
+const API_HOST = 'https://0b9b-106-51-159-190.ngrok-free.app';
 
 // Fetch trending articles
 export const fetchTrendingArticles = createAsyncThunk('articles/fetchTrending', async () => {
