48ef905252e1051edee344b094ee4be27c0fc954
Ashok naik <ashok@yourstory.com>
2025-07-03 16:23:53 +0530

    Fix URL space bug in API endpoint

--- src/features/articles/articlesSlice.js
+++ src/features/articles/articlesSlice.js
@@ -275,7 +275,7 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 // TODO: Move to Env file
-const API_HOST = ' https://b4d6-106-51-159-190.ngrok-free.app ';
+const API_HOST = 'https://b4d6-106-51-159-190.ngrok-free.app';
 
 // Fetch trending articles
 export const fetchTrendingArticles = createAsyncThunk('articles/fetchTrending', async () => {
