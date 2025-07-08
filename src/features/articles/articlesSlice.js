23160efe79cf4cb83b0c28456bd2a043e4c3a211
Ashok naik <ashok@yourstory.com>
2025-07-08 16:50:38 +0530

    Fix: Updated API host and article fetch logic

--- src/features/articles/articlesSlice.js
+++ src/features/articles/articlesSlice.js
@@ -275,11 +275,11 @@ import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
 // TODO: Move to Env file
-const API_HOST = 'https://3e65-106-51-159-190.ngrok-free.app';
+const API_HOST = 'https://6d9e8ffd3818.ngrok-free.app';
 
 // Fetch trending articles
 export const fetchTrendingArticles = createAsyncThunk('articles/fetchTrending', async () => {
-  const response = await axios.get(`${API_HOST}/api/articles/trending`);
+  const response = await axios.get(`${API_HOST}/api/articles/v2/trending`);
   return Array.isArray(response.data) ? response.data : [];
 });
 
